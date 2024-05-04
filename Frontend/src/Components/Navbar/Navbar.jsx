a01b4b329016611795c232e8ce70b9cee22f1fe7
Shubham Surve <118058459+shub-surve@users.noreply.github.com>
2024-05-04 12:05:46 +0530

    Changes in Navbar-added For Business Button
    pending-onclick event

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from './Dropdown';
 import Searchbar from './Searchbar';
+import { IoIosBusiness } from "react-icons/io";
+
 const Navbar = () => {
   const navItems = [
-    { to: '/companies', text: 'Companies' },
-    { to: '/tasks', text: 'Tasks' },
-    { to: '/tests', text: 'Tests' },
-    { to: '/FAQ', text: 'FAQ' },
-    { to: '/about', text: 'About' },
+    { to: '/learn', text: 'Learn' },
+    { to: '/practice', text: 'Practice' },
+    { to: '/mentorship', text: 'Mentorship' },
+    { to: '/compete', text: 'Compete' },
+    { to: '/jobs', text: 'Jobs' },
   ];
 
   return (
@@ -18,12 +20,9 @@ const Navbar = () => {
           {/* Logo */}
           <div className="flex items-center">
             <img className="h-12 mr-4" src="./Imgs/logo.png" alt="Logo" />
-            <span className="text-gray-700 font-semibold text-lg">
-              learn&leap
-            </span>
+            <span className="text-gray-700 font-semibold text-lg">unstop</span>
           </div>
-          <Searchbar/>
-
+          <Searchbar />
           {/* Navigation Links */}
           <ul className="hidden md:flex items-center space-x-4">
             {navItems.map((item, index) => (
@@ -43,12 +42,18 @@ const Navbar = () => {
               >
                 Login
               </Link>
+              <Link>
               <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition duration-300">
-                Join for Free
+                Join us
+              </button>
+              </Link>
+              <button className="bg-yellow-600 text-white font-semibold py-2 px-4 rounded hover:bg-blue-300 transition duration-300 flex border-1 border-yellow-100 " 
+              >
+                <IoIosBusiness className='flex mt-1 '/>
+                For Business
               </button>
             </div>
           </ul>
-
           {/* Mobile Dropdown Menu */}
           <div className="md:hidden">
             <Dropdown items={navItems} />
